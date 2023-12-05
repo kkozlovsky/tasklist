283ae467e7874270e12f2c2025d6ea890d26c4be
Kirill Kozlovskiy <k.kozlovskiy@paysend.com>
2023-12-05 22:36:05 +0300

    Initial commit

---
+++ src/test/java/ru/kerporation/tasklist/TasklistApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kerporation.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
