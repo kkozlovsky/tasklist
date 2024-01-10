e251fff9ca952b090d48d7d8205f368331a34798
kkozlovsky <kkozlovsky@yandex.ru>
2024-01-10 20:18:42 +0300

    Validations

--- src/main/java/ru/kerporation/tasklist/web/dto/auth/JwtRequest.java
+++ src/main/java/ru/kerporation/tasklist/web/dto/auth/JwtRequest.java
@@ -1,9 +1,14 @@
 package ru.kerporation.tasklist.web.dto.auth;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class JwtRequest {
+
+    @NotNull(message = "Username must be not null")
     private String username;
+
+    @NotNull(message = "Password must be not null")
     private String password;
 }
--- src/main/java/ru/kerporation/tasklist/web/dto/task/TaskDto.java
+++ src/main/java/ru/kerporation/tasklist/web/dto/task/TaskDto.java
@@ -1,15 +1,32 @@
 package ru.kerporation.tasklist.web.dto.task;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 import ru.kerporation.tasklist.domain.task.Status;
+import ru.kerporation.tasklist.web.dto.validation.OnCreate;
+import ru.kerporation.tasklist.web.dto.validation.OnUpdate;
 
 import java.time.LocalDateTime;
 
 @Data
 public class TaskDto {
+
+    @NotNull(message = "Id must be not null", groups = OnUpdate.class)
     private Long id;
+
+    @NotNull(message = "Title must be not null", groups = {OnCreate.class, OnUpdate.class})
+    @Length(max = 255, message = "Title length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String title;
+
+    @Length(max = 255, message = "Description length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String description;
+
     private Status status;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime expirationDate;
 }
