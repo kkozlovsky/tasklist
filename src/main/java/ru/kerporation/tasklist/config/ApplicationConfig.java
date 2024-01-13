2e464183b1964408cee1dd1b013754301333602f
kkozlovsky <kkozlovsky@yandex.ru>
2024-01-13 23:45:57 +0300

    Fix cycle dependency bug

--- src/main/java/ru/kerporation/tasklist/config/ApplicationConfig.java
+++ src/main/java/ru/kerporation/tasklist/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -20,7 +21,7 @@ import ru.kerporation.tasklist.web.security.JwtTokenProvider;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Lazy))
 public class ApplicationConfig {
 
     private final ApplicationContext applicationContext;
