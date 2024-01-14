44f32758a7a450746c2168eba15c86554de97dc2
kkozlovsky <kkozlovsky@yandex.ru>
2024-01-14 11:26:02 +0300

    ControllerAdvice

---
+++ src/main/java/ru/kerporation/tasklist/domain/exception/ExceptionBody.java
@@ -0,0 +1,17 @@
+package ru.kerporation.tasklist.domain.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ExceptionBody {
+    private String message;
+    private Map<String, String> errors;
+
+    public ExceptionBody(final String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/ru/kerporation/tasklist/web/controller/advice/ControllerAdvice.java
@@ -0,0 +1,75 @@
+package ru.kerporation.tasklist.web.controller.advice;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.kerporation.tasklist.domain.exception.AccessDeniedException;
+import ru.kerporation.tasklist.domain.exception.ExceptionBody;
+import ru.kerporation.tasklist.domain.exception.ResourceMappingException;
+import ru.kerporation.tasklist.domain.exception.ResourceNotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class ControllerAdvice {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionBody handleResourceNotFound(final ResourceNotFoundException e) {
+        return new ExceptionBody(e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceMappingException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionBody handleResourceMapping(final ResourceMappingException e) {
+        return new ExceptionBody(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleIllegalState(final IllegalStateException e) {
+        return new ExceptionBody(e.getMessage());
+    }
+
+    @ExceptionHandler({AccessDeniedException.class, org.springframework.security.access.AccessDeniedException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ExceptionBody handleAccessDenied() {
+        return new ExceptionBody("Access denied.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        final ExceptionBody exceptionBody = new ExceptionBody("Validation failed.");
+        final List<FieldError> errors = e.getBindingResult().getFieldErrors();
+        exceptionBody.setErrors(errors.stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)));
+        return exceptionBody;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleConstraintViolation(final ConstraintViolationException e) {
+        final ExceptionBody exceptionBody = new ExceptionBody("Validation failed.");
+        exceptionBody.setErrors(e.getConstraintViolations().stream()
+                .collect(Collectors.toMap(
+                        violation -> violation.getPropertyPath().toString(),
+                        violation -> violation.getMessage()
+                )));
+        return exceptionBody;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionBody handleException(final Exception e) {
+        log.error(e.getMessage(), e);
+        return new ExceptionBody("Internal error.");
+    }
+}
